683b4bb55b70c0a3708d7cb58b1869f54103831b
SANJAY BADWAIK <snjbadwaik96@gmail.com>
2023-06-17 13:10:31 +0530

    first day -navbar completed

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDiEz7kW8_3eNaDshOhOSax93t4vT2d8x8",
+  authDomain: "e-clone-4ce43.firebaseapp.com",
+  projectId: "e-clone-4ce43",
+  storageBucket: "e-clone-4ce43.appspot.com",
+  messagingSenderId: "552237397660",
+  appId: "1:552237397660:web:14fa9df47756eac9c01f59",
+  measurementId: "G-7YMNFYQG8T"
+};
